e643f048ad1c91be6efe8e005dcfc9b8691cf628
Baptisteuh <77227564+Baptisteuh@users.noreply.github.com>
2024-12-15 15:07:16 +0100

    added controls scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -16,7 +16,7 @@ func _on_start_button_pressed() -> void:
 
 
 func _on_controls_button_pressed() -> void:
-	print("controls")
+	get_tree().change_scene_to_file("res://scenes/controls.tscn")
 
 
 func _on_quit_button_pressed() -> void:
